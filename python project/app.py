53a04687760425f4c33d0b987fe01c9ef85294cf
igors1lva <igorfernandosilva3@gmail.com>
2026-09-18 23:54:38 +0000

    Adicionado Flask app e elementos no arquivo para passar ao HTML template for user display

---
+++ python project/app.py
@@ -0,0 +1,15 @@
+from Flask import flask, render_template
+app = Flask(__name__)
+@app.route("/")
+def home():
+    #dados que serao! calculados python
+    titulo_pagina = "Face Page"
+    aluno = {"nome": "João", "curso": "Python", "nota": 8.5}
+    status = "Aprovado" if aluno["nota"] >= 5 else "Reprovado"
+    #passando dados para o template HTML
+    return render_template("index.html", 
+                           titulo=titulo_pagina, 
+                           usuario=aluno, 
+                           resultado=status)
+if __name__ == "__main__":
+    app.run(debug=True)
