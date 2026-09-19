2c066dbb35d69b3154be79a812775b46bdcdfd9c
igors1lva <igorfernandosilva3@gmail.com>
2026-09-19 00:56:31 +0000

    Fix capitalization of status messages in home function

--- python project/app.py
+++ python project/app.py
@@ -5,7 +5,7 @@ def home():
     #dados que serao! calculados python
     titulo_pagina = "Face Page"
     aluno = {"nome": "João", "curso": "Python", "nota": 4.5}
-    status = "aprovado" if aluno["nota"] >= 5 else "reprovado"
+    status = "Aprovado" if aluno["nota"] >= 5 else "Reprovado"
     #passando dados para o template HTML
     return render_template("index.html", 
                            titulo=titulo_pagina, 
